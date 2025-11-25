74a8fee45a83e9166ac49f43718fae68a11658f5
sebmaz <seb.maz93@icloud.com>
2025-11-26 03:19:42 +0400

    Add mux handler and file server

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Yoo")
+	const port = "8080"
+	const rootPath = "."
+
+	mux := http.NewServeMux()
+	dir := http.Dir(rootPath)
+	fileServer := http.FileServer(dir)
+	mux.Handle("/", fileServer)
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start the server!")
+	}
+	log.Printf("Server listening on port :%s\n", port)
 }
